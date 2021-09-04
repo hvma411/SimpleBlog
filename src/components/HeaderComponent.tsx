d66475edac67bda377904eb94c84924bf2016a2c
hvma411 <kamil.nosek411@gmail.com>
2021-09-04 18:42:12 +0200

    initial commit with simple configuration and part of header

---
+++ src/components/HeaderComponent.tsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+
+const HeaderComponent = () => {
+    return (
+        <header className="header">
+            <h1 className="header-title">
+                Simple Blog
+            </h1>
+        </header>
+    )
+}
+
+export default HeaderComponent
